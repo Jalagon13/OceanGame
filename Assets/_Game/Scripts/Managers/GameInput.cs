130eb134e80eb4b87b46c2773d64f8530cacfa24
Jalagon <A1ag0nJus1in@gmail.com>
2026-08-18 17:45:25 -0700

    2D Edits

--- Assets/_Game/Scripts/Managers/GameInput.cs
+++ Assets/_Game/Scripts/Managers/GameInput.cs
@@ -2,7 +2,7 @@ using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-namespace LittleGuyGamePrototype
+namespace OceanGame
 {
     public class GameInput : MonoBehaviour
     {
--- Assets/_Game/Scripts/StateMachine/State.cs
+++ Assets/_Game/Scripts/StateMachine/State.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace LittleGuyGamePrototype
+namespace OceanGame
 {
     public abstract class State 
     {
